134e4aa845d48a3f88d2ab9d3012772bec470d19
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-04-18 15:15:23 +0530

    set page2

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,33 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Home from './components/main-pages/Home';
 import Page2 from './components/main-pages/Page2';
 import LocomotiveScroll from 'locomotive-scroll';
+import Page3 from './components/main-pages/page3';
 
 function App() {
   gsap.registerPlugin(useGSAP,ScrollTrigger);
-  const locomotiveScroll = new LocomotiveScroll({
-    lenisOptions: {
-      autoResize: true,
-      smoothScrolling: true,
-      wrapper: window,
-      content: document.documentElement,
-      lerp: 0.1,
-      duration: 3,
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      smoothWheel: true,
-      smoothTouch: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-      normalizeWheel: true,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    },
-  });
+  // const locomotiveScroll = new LocomotiveScroll({
+  //   lenisOptions: {
+  //     autoResize: true,
+  //     smoothScrolling: true,
+  //     wrapper: window,
+  //     content: document.documentElement,
+  //     lerp: 0.1,
+  //     duration: 3,
+  //     orientation: "vertical",
+  //     gestureOrientation: "vertical",
+  //     smoothWheel: true,
+  //     smoothTouch: true,
+  //     wheelMultiplier: 1,
+  //     touchMultiplier: 2,
+  //     normalizeWheel: true,
+  //     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  //   },
+  // });
   return (
     <div className='w-full h-screen text-white select-none bg-black '>
       <Home/>
       <Page2/>
+      {/* <Page3/> */}
     </div>
   )
 }
--- src/components/main-pages/Page2.jsx
+++ src/components/main-pages/Page2.jsx
@@ -3,7 +3,7 @@ import Page2setup from '../partials/Page2-setup'
 
 function Page2() {
   return (
-    <div  className='w-full h-[100vh] bg-black flex items-center justify-center p-10'>
+    <div  className='w-full h-[100vh]  bg-black flex items-center relative top-0 left-0 justify-center p-2'>
         <Page2setup/>
     </div>
   )
