b80da6a999b851424ed1b1a7912aae89d37e4fb9
Adarshkumarjhook <amitkumarjhookiya@gmail.com>
2024-04-20 02:40:02 +0530

    page_2

--- src/components/partials/Page2-setup.jsx
+++ src/components/partials/Page2-setup.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Box from '../partials/Box';
+import Box_1 from '../partials/Box_1';
 
 function Page2setup() {
-  return (
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isMobile == false ? (
     <div className="w-full min-h-full text-white overflow-hidden">
       <div className="text-center absolute boxs w-full top-[7vw] -left-[2.7vw] heading ">
         <h1 className="text-7xl shadows shadow-white">Features</h1>
@@ -104,7 +117,85 @@ function Page2setup() {
         <div className="w-[2px] h-[5.7vw] absolute boxs bg-white boxs-line top-[4.96vw] left-[18vw] rotate-[-52deg]"></div>
       </div>
     </div>
-  );
-}
 
+  ) : (
+    <>
+    <div className="w-[100vw] relative flex gap-[2vw] p-1  h-[100vh] min-h-[100vh] text-white bg-white  overflow-hidden">
+    
+    <div className="relative rounded-2xl h-full w-[50vw] bg-black "></div>
+     <div className="relative rounded-2xl h-full w-[50vw]  bg-black"></div>
+    
+    
+    
+      <div className="absolute top-[17.5vw] left-[5vw]"> 
+      <div className="relative w-full h-full ">
+      <Box w="25vw"  type="type_1"/>
+      <div className="absolute boxs top-[2vw] w-[16vw] left-[1vw] text-3 z-30 flex flex-col gap-1 ">
+          <h1 className="text-xs np">Chipset</h1>
+          <p className="text-[2vw]">Snapdragon 778G+ accelerated by 5G</p>
+        </div>
+      </div>
+      </div>
+    
+        <div className="absolute top-[37vw] left-[4vw]"> 
+      <div className="relative w-full h-full">
+      <Box_1 w="20vw"  type="type_1"/>
+      </div>
+      </div>
+    
+     <div className="absolute top-[25.5vw] left-[25vw]"> 
+      <div className="relative w-full h-full">
+      <Box_1 w="20vw"  type="type_4"/>
+
+      </div>
+      </div>
+      
+       <div className="absolute top-[46.5vw] left-[20vw]"> 
+      <div className="relative w-full h-full">
+      <Box w="25vw"  type="type_4"/>
+      </div>
+      </div>
+    
+    
+    
+    
+    
+    
+          <div className="absolute top-[15.5vw] right-[10.5vw]"> 
+      <div className="relative w-full h-full">
+      <Box_1 w="20vw"  type="type_2"/>
+      </div>
+      </div>
+    
+        <div className="absolute top-[37vw] right-[4vw]"> 
+      <div className="relative w-full h-full">
+      <Box_1 w="20vw"  type="type_2"/>
+      </div>
+      </div>
+    
+     <div className="absolute top-[25.5vw] right-[25vw]"> 
+      <div className="relative w-full h-full">
+      <Box_1 w="20vw"  type="type_3"/>
+      </div>
+      </div>
+      
+       <div className="absolute top-[46.5vw] right-[20vw]"> 
+      <div className="relative w-full h-full">
+      <Box w="25vw"  type="type_3"/>
+      </div>
+      </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      
+      </div>
+      </>
+  )
+}
 export default Page2setup;
