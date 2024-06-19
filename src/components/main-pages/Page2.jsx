f3d801930227118de61f1d9e055ba57bb08af31c
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-06-19 11:43:20 +0530

    efws

--- src/components/main-pages/Page2.jsx
+++ src/components/main-pages/Page2.jsx
@@ -7,29 +7,30 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 function Page2() {
   gsap.registerPlugin(useGSAP,ScrollTrigger);
 
-//   const tl = gsap.timeline({scrollTrigger: {
-//     trigger: ".page2",
-//     start: "top 0%",
-//     // end: "bottom bottom",
-//     // scrub: true,
-//     boxShadow: "0px 0px 20px #fff",
-// }});
+  const tl = gsap.timeline({scrollTrigger: {
+    trigger: ".page2",
+    start: "top 0%",
+    // end: "bottom bottom",
+    matches:true,
+    // scrub: true,
+    boxShadow: "0px 0px 20px #fff",
+}});
 
-//   useGSAP(() => {
-//     tl.from(".text-2",{
-//       opacity:0,
-//       duration: 2
-//     })
-//     tl.from(".boxs",{
-//       opacity: 0,
-//       // y: -300,
-//       width:0,
-//       height:0,
-//       // scrollBehavior:smooth ,
-//       stagger: 0.1
-//     })
+  useGSAP(() => {
+    tl.from(".text-2",{
+      opacity:0,
+      duration: 2
+    })
+    tl.from(".boxs",{
+      opacity: 0,
+      // y: -300,
+      width:0,
+      height:0,
+      // scrollBehavior:smooth ,
+      stagger: 0.1
+    })
     
-//   })
+  })
   return (
     <div  className='w-full  page2 page h-[100vh] overflow-hidden bg-black flex items-center relative top-0 left-0 justify-center p-2'>
         <Page2setup/>
