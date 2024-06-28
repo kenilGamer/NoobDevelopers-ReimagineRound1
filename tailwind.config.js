01e074a932f82ade48caa3488c1b3c8b61b46d03
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-06-28 23:28:05 +0530

    ff

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
+   
   },
   plugins: [],
 }
