91905d8dc2ddd3915fc0877b65dc0a1a0274d071
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-07-01 15:20:48 +0530

    FVFV

--- src/components/main-pages/page5.jsx
+++ src/components/main-pages/page5.jsx
@@ -68,20 +68,20 @@ function Page5({rotate}) {
       {isMobile == false ? (
         <div   className='camera-page'>
           
-          <p   className='absolute z-[50] text-2-1 top-[22vh] left-[50vw] text-5xl font-black w-[40vw] text-white  heading font2'>Include yourself with every moment</p>
-          <p   className='absolute z-[50] text-2-1 top-[75vh] left-[50vw] font-black text-3xl w-[40vw] text-white font-mono tracking-tighter'>Say nothing and have a click</p>
+          <p   className='absolute z-[50] text-2-1 top-[22vh] left-[50vw] text-5xl font-black w-[40vw] text-[#999d9c]  heading font2'>Include yourself with every moment</p>
+          <p   className='absolute z-[50] text-2-1 top-[75vh] left-[50vw] font-black text-3xl w-[40vw] text-[#999d9c] font-mono tracking-tighter'>Say nothing and have a click</p>
          
          <div className='absolute left-2  w-full'>
          <h1  className='absolute z-[50] text-1 top-[14vh] left-[18.5vw] w-32'>16 MP Sony IMX471 sensor</h1>
-          <h1  className='absolute z-[50] text-1 top-[25vh] left-[17vw] text-sm heading'>Front camera</h1>
-          <div className='absolute boxs1 z-[50] h-[1px] w-[6vw] top-[17.5vh] left-[27vw] bg-white'></div>
-          <div className='absolute boxs1 z-[50] h-[2px] w-[7.5vw] top-[13.5vh] left-[18.5vw] bg-white'></div>
-          <div className='absolute boxs1 z-[50] h-[2px] w-[1.5vw] top-[14.8vh] left-[25.7vw] bg-white rotate-[45deg]'></div>
-          <div className='absolute boxs1 z-[50] h-[2px] w-[2vw] top-[17.8vh] left-[26vw] bg-white rotate-[90deg]'></div>
-          <div className='absolute boxs1 z-[50] h-[2px] w-[1.5vw] top-[20.8vh] left-[25.7vw] bg-white -rotate-[45deg]'></div>
-          <div className='absolute boxs1 z-[50] h-[2px] w-[8.6vw] top-[21.9vh] left-[17.4vw] bg-white'></div>
-          <div className='absolute boxs1 z-[50] h-[2px] w-[2.5vw] top-[19.4vh] left-[16vw] bg-white rotate-[90deg]'></div>
-          <div className='absolute boxs1 z-[50] h-[2px] w-[2vw] top-[15vh] left-[16.9vw] bg-white -rotate-[45deg]'></div>
+          <h1  className='absolute z-[50] text-1 top-[25vh] left-[17vw] text-sm heading text-[#cfd4d3]'>Front camera</h1>
+          <div className='absolute boxs1 z-[50] h-[1px] w-[6vw] top-[17.5vh] left-[27vw] bg-[#999d9c]'></div>
+          <div className='absolute boxs1 z-[50] h-[2px] w-[7.5vw] top-[13.5vh] left-[18.5vw] bg-[#999d9c]'></div>
+          <div className='absolute boxs1 z-[50] h-[2px] w-[1.5vw] top-[14.8vh] left-[25.7vw] bg-[#999d9c] rotate-[45deg]'></div>
+          <div className='absolute boxs1 z-[50] h-[2px] w-[2vw] top-[17.8vh] left-[26vw] bg-[#999d9c] rotate-[90deg]'></div>
+          <div className='absolute boxs1 z-[50] h-[2px] w-[1.5vw] top-[20.8vh] left-[25.7vw] bg-[#999d9c] -rotate-[45deg]'></div>
+          <div className='absolute boxs1 z-[50] h-[2px] w-[8.6vw] top-[21.9vh] left-[17.4vw] bg-[#999d9c]'></div>
+          <div className='absolute boxs1 z-[50] h-[2px] w-[2.5vw] top-[19.4vh] left-[16vw] bg-[#999d9c] rotate-[90deg]'></div>
+          <div className='absolute boxs1 z-[50] h-[2px] w-[2vw] top-[15vh] left-[16.9vw] bg-[#999d9c] -rotate-[45deg]'></div>
          </div>
         </div>
       ) : (
@@ -93,7 +93,7 @@ function Page5({rotate}) {
             <div className='absolute w-full text-center sn top-0'>
               <h1 className='text-[2.5vw] page5-2 text-1'>16 MP Sony IMX471 sensor</h1>
             </div>
-            <div className='absolute -top-[4.6vw] -right-[7vw] w-[1px] h-[16vw] rotate-90 bg-white'></div>
+            <div className='absolute -top-[4.6vw] -right-[7vw] w-[1px] h-[16vw] rotate-90 bg-[#999d9c]'></div>
           </div>
         </div>
        </div>
